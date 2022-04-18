3d1a9d1eec7b9c4c68a355f9f07394069eed3250
Hanna Serdjuk <92991892+Hanna-Serdjuk@users.noreply.github.com>
2022-04-18 11:26:12 +0300

    task #4

--- js/task-04.js
+++ js/task-04.js
@@ -4,3 +4,19 @@
 // Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
 // Обновляй интерфейс новым значением переменной counterValue.
 
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+let counterValue = document.querySelector('#value');
+
+const onIncrementBtnClick = () => {
+
+    counterValue.textContent = Number(counterValue.textContent) + 1;
+}
+
+const onDecrementBtnClick = () => {
+
+    counterValue.textContent = Number(counterValue.textContent) - 1;
+}
+
+incrementBtn.addEventListener('click', onIncrementBtnClick);
+decrementBtn.addEventListener('click', onDecrementBtnClick);
